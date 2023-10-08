312b839c3d9e25b4079cbfd6f3cdefed524bd5d5
Ruskin Kollin <ruskin.kollin@sa-tu.com>
2023-10-08 23:05:36 +0300

    Food app project started.

---
+++ food_app/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,23 @@
+import DUMMY_MEALS from './dummyMeals';
+import Card from '../UI/Card';
+import MealItem from './MealItem.js/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+    return (
+        <section className={classes.summary}>
+            <Card>
+                <ul>
+                    {DUMMY_MEALS.map((meal) =>
+                        <MealItem
+                            key={meal.id}
+                            name={meal.name}
+                            description={meal.description}
+                            price={meal.price} />)}
+                </ul>
+            </Card>
+        </section>
+    )
+};
+
+export default AvailableMeals;
