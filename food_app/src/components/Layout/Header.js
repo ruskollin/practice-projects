367ff4117dea6d29d34284a2cbfeb057c6798208
Ruskin Kollin <ruskin.kollin@sa-tu.com>
2023-10-09 22:11:33 +0300

    Food app with reducer done.

--- food_app/src/components/Layout/Header.js
+++ food_app/src/components/Layout/Header.js
@@ -6,8 +6,8 @@ const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <h1>Ruskin's Restaurant</h1>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='Table full of food' />
--- food_app/src/components/Meals/MealItem.js/MealItem.js
+++ food_app/src/components/Meals/MealItem.js/MealItem.js
@@ -1,8 +1,23 @@
+import { useContext } from 'react';
+
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
+const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
 
-const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        });
+    };
+
     return (
         <li className={classes.meal}>
             <div>
@@ -11,10 +26,10 @@ const MealItem = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
-    )
+    );
 };
 
 export default MealItem;
